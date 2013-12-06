81e67c7f7afd89a4f3e0d37f114f9545e304f927
Olivier Bruchez <olivier@bruchez.org>
2013-12-07 00:46:49 +0100

    Remove Lift version

--- src/main/scala/org/bruchez/olivier/model/xml/Concerts.scala
+++
@@ -1,5 +0,0 @@
-package org.bruchez.olivier.model.xml
-
-object Concerts {
-  // @todo import from XML file into database
-}
--- src/main/scala/org/bruchez/olivier/snippet/Home.scala
+++
@@ -1,8 +0,0 @@
-package org.bruchez.olivier.snippet
-
-import net.liftweb.http._
-import xml.NodeSeq
-
-object Home {
-  def render: NodeSeq = S.redirectTo("http://bruchez.blogspot.com/")
-}
