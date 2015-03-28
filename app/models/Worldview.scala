87ad35c91d5cc3f91d7b5a741d939c06f6ed9492
Olivier Bruchez <olivier@bruchez.org>
2015-03-28 19:52:11 +0100

    Add worldview (built from XML source)

--- app/models/Worldview.scala
+++ app/models/Worldview.scala
@@ -1,10 +1,8 @@
 package models
 
-import com.github.rjeschke.txtmark._
 import java.net.URL
-import scala.collection.mutable
-import scala.io.Source
 import scala.util.Try
+import scala.xml._
 import util.HtmlContent
 
 case class WorldviewPosition(summary: HtmlContent, details: HtmlContent)
@@ -15,104 +13,34 @@ case class Worldview(worldviewCategories: Seq[WorldviewCategory], references: Se
 
 object Worldview {
   def apply(url: URL): Try[Worldview] = for {
-    markdown <- Try(Source.fromURL(url).mkString)
-    worldview <- apply(markdown)
-  } yield worldview
+     xml <- Try(XML.load(url))
+     profile <- apply(xml)
+   } yield profile
 
-  // @todo use XML source instead...?
+  def apply(elem: Elem): Try[Worldview] = Try {
+     val worldview = (elem \\ "worldview").head
 
-  def apply(markdown: String): Try[Worldview] = Try {
-    //val html = Processor.process(markdown)
+     val worldviewCategories = for {
+       category <- worldview \\ "category"
+       descriptionAsMarkdown = category \@ "description"
+     } yield {
+         val worldviewPositions = for {
+           position <- category \\ "position"
+           summaryAsMarkdown = position \@ "summary"
+           detailsAsMarkdown = position.text
+         } yield WorldviewPosition(
+           summary = HtmlContent.fromMarkdown(summaryAsMarkdown).get,
+           details = HtmlContent.fromMarkdown(detailsAsMarkdown).get)
 
-    val nonEmptyLines = markdown.split("\\r?\\n").toSeq.map(_.trim).filter(_.nonEmpty)
+         WorldviewCategory(description = HtmlContent.fromMarkdown(descriptionAsMarkdown).get, worldviewPositions)
+     }
 
-    val linesGroupedByHeaders: Seq[(String, Seq[String])] =
-      linesByH2Headers(nonEmptyLines) filterNot { case (h2Header, _) =>
-        H2HeadersToIgnore.contains(h2Header.toLowerCase)
-      }
+    val references = for {
+      references <- worldview \\ "references"
+      reference <- references \\ "reference"
+      referenceAsMarkdown = reference.text
+    } yield HtmlContent.fromMarkdown(referenceAsMarkdown).get
 
-    val references: Seq[String] =
-      linesGroupedByHeaders.find(_._1.toLowerCase == ReferencesH2Header).map(_._2).getOrElse(Seq())
-
-    println(s"references = $references")
-
-    Worldview(Seq(), Seq())
-  }
-
-  private def linesByH2Headers(lines: Seq[String]): Seq[(String, Seq[String])] =
-    linesByHeaders(lines, headerLevel = 2)
-
-  private def linesByH5Headers(lines: Seq[String]): Seq[(String, Seq[String])] =
-    linesByHeaders(lines, headerLevel = 5)
-
-  private def linesByHeaders(lines: Seq[String], headerLevel: Int): Seq[(String, Seq[String])] = {
-    val headerPrefix = "#" * headerLevel + " "
-
-    val result = mutable.Buffer[(String, Seq[String])]()
-    var header = ""
-    var headerLines = Seq[String]()
-
-    def accumulate(headerLineOption: Option[String]): Unit = {
-      result.append((header, headerLines))
-      headerLineOption foreach { headerLine =>
-        header = headerLine.substring(headerPrefix.length)
-        headerLines = Seq()
-      }
-    }
-
-    for (line <- lines) {
-      if (line.startsWith(headerPrefix)) {
-        accumulate(headerLineOption = Some(line))
-      } else {
-        headerLines = headerLines :+ line
-      }
-    }
-
-    accumulate(headerLineOption = None)
-
-    result.toSeq
-  }
-
-  private val ThemesToAddH2Header = "thèmes à ajouter"
-  private val IdeasToAddH2Header = "idées à ajouter"
-  private val H2HeadersToIgnore = Set(ThemesToAddH2Header, IdeasToAddH2Header)
-
-  private val ReferencesH2Header = "références générales"
+     Worldview(worldviewCategories, references)
+   }
 }
-
-/*
-## Explication du monde ("Quelle est la nature de notre univers ? Comment fonctionne-t-il ?")
-
-##### Il existe une réalité matérielle objective, unique, dans laquelle nous existons et avons nos expériences subjectives.
-
-J'adhère donc à un point de vue [réaliste](http://fr.wikipedia.org/wiki/R%C3%A9alisme_%28philosophie%29) et [matérialiste](http://fr.wikipedia.org/wiki/Mat%C3%A9rialisme) (ou, pour être un peu plus précis, [physicaliste](http://fr.wikipedia.org/wiki/Physicalisme) et [moniste](http://fr.wikipedia.org/wiki/Monisme)). J'adhère également et en particulier à la position que [Paul Thagard](https://en.wikipedia.org/wiki/Paul_Thagard) appelle le [réalisme constructif](http://philoscience.over-blog.com/article-the-brain-and-the-meaning-of-life-par-pa-51282235.html). Ce point de vue détermine à lui seul toute une série de concepts en lesquels je ne crois pas, car, de manière générale, pour moi, il n'existe rien en dehors de cette réalité matérielle dont il est question. Je ne crois par exemple pas en Dieu ([athéisme](http://fr.wikipedia.org/wiki/Ath%C3%A9isme)). De même, je ne crois pas en l'existence de l'âme (absence de [dualisme](https://fr.wikipedia.org/wiki/Dualisme_(philosophie_de_l%27esprit))) ; je crois que la conscience est une propriété émergente de notre cerveau (ce qui, en soi, n'explique rien et demeure, pour l'instant, un "mystère"). Je ne crois toutefois pas forcément que l'être humain pourra un jour comprendre l'entier de la réalité dans laquelle il évolue. En tout cas pas en se contentant de son seul cerveau. Je pense en effet que les ordinateurs et l'intelligence artificielle joueront un rôle fondamental dans la compréhension de l'univers.
-
-##### L'univers dans lequel nous vivons est déterministe.
-...
-
-## Références générales
-
-* [ebruchez's world view](https://github.com/ebruchez/public/blob/master/Wordview.md)
-* [World Views: From fragmentation to integration](http://www.vub.ac.be/CLEA/pub/books/worldviews.pdf)
-* [World view development](https://en.wikipedia.org/wiki/World_view#Development)
-
-## Thèmes à ajouter
-
- * armée, guerre
- * armes (port d'armes, etc.)
- * drogues (légalisation, cannabis, etc)
- * droits des enfants (éducation, travail, etc.)
- * droits des femmes (avortement, [prostitution](http://danielmiessler.com/blog/current-view-prostitution/), etc.)
- * droits des homosexuels (mariage, adoption, etc.)
- * état (lien état / individu, lien état / économie, etc.)
- * finance
- * justice (prison, réhabilitation, etc.)
- * liberté d'expression, de la presse, de blasphémer (cf Charlie Hebdo)
- * politique (démocratie, systèmes politiques, etc.)
- * santé (assurances, assistance au suicide, etc.)
- * vin, audiophilie (effet placebo et snobisme)
- * voir aussi ["What do philosophers believe?"](http://philpapers.org/archive/BOUWDP)
-
-## A faire / idées à ajouter
-...
- */
--- app/util/HtmlContent.scala
+++ app/util/HtmlContent.scala
@@ -1,3 +1,21 @@
 package util
 
+import com.github.rjeschke.txtmark._
+import scala.util._
+
 case class HtmlContent(string: String)
+
+object HtmlContent {
+  def fromMarkdown(markdown: String): Try[HtmlContent] = Try {
+    HtmlContent(string = withoutRootParagraph(Processor.process(markdown)))
+  }
+
+  private def withoutRootParagraph(html: String): String = {
+    val trimmedHtml = html.trim
+
+    if (trimmedHtml.startsWith("<p>") && trimmedHtml.endsWith("</p>"))
+      trimmedHtml.substring("<p>".length, trimmedHtml.length - "</p>".length)
+    else
+      html
+  }
+}
