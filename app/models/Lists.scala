ac762b8c4573d1ab0976122b513624b4f854d09f
Olivier Bruchez <olivier@bruchez.org>
2015-03-29 16:38:16 +0200

    Add books/concerts counts

--- app/models/Lists.scala
+++ app/models/Lists.scala
@@ -17,26 +17,6 @@ object Lists {
   def isTrue(string: String): Boolean =
     Set("on", "true", "y", "yes", "1").contains(string.trim.toLowerCase)
 
-  /*
-      <xsl:template name="comma-or-and">
-        <xsl:choose>
-            <xsl:when test="position()=last()-1">
-                <xsl:choose>
-                    <xsl:when test="last()=2">
-                        <xsl:text> and </xsl:text>
-                    </xsl:when>
-                    <xsl:otherwise>
-                        <xsl:text>, and </xsl:text>
-                    </xsl:otherwise>
-                </xsl:choose>
-            </xsl:when>
-            <xsl:when test="position()!=last()">
-                <xsl:text>, </xsl:text>
-            </xsl:when>
-        </xsl:choose>
-    </xsl:template>
-   */
-
   def commaOrAnd(index: Int, totalCount: Int): String =
     if (index == totalCount - 2)
       if (totalCount == 2) " and " else ", and"
