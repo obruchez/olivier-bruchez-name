55d1d426dd1e037e99e080c92774ca4d6167ef8e
Olivier Bruchez <olivier@bruchez.org>
2015-04-11 18:05:21 +0200

    Serve course certificates from cache

--- app/controllers/CourseCertificatesController.scala
+++ app/controllers/CourseCertificatesController.scala
@@ -4,26 +4,31 @@ import actors.Cache
 import models.{Courses, CourseCertificate}
 import play.api.libs.concurrent.Execution.Implicits._
 import play.api.mvc._
-import scala.util._
+import scala.concurrent.Future
 import util._
 
 object CourseCertificatesController extends Controller {
   def courseCertificate(slug: String) = Action.async {
-    Cache.get(Courses) map { courses =>
-      courses.certificateFromSlug(slug) match {
-        case Some(certificate) =>
-          // We expect only binary file types (PDF, mainly)
-          BinaryContent(certificate.url) match {
-            case Success(binaryContent) =>
-              Ok(binaryContent.content).as(binaryContent.fileType.mimeType)
-            case Failure(throwable) =>
-              Ok(views.html.error(courses.pageFromCertificate(certificate), throwable))
-          }
-        case None =>
-          NotFound
-      }
+      for {
+        courses <- Cache.get(Courses)
+        result <- resultFromSlug(courses, slug)
+      } yield result
     }
-  }
+
+    private def resultFromSlug(courses: Courses, slug: String): Future[Result] = courses.certificateFromSlug(slug) match {
+      case Some(certificate) =>
+        val page = courses.pageFromCertificate(certificate)
+
+        Cache.fileContent(certificate.fileSource) map {
+          case binaryContent: BinaryContent =>
+            // We expect only binary file types (PDF, mainly)
+            Ok(binaryContent.content).as(binaryContent.fileType.mimeType)
+          case _ =>
+            NotImplemented
+        }
+      case None =>
+        Future(NotFound)
+    }
 
   implicit class CoursesOps(courses: Courses) {
     def pageFromCertificate(courseCertificate: CourseCertificate): Page = {
