bc1d98e3984ee9c4625d1f58279f6358f9bc2db2
Olivier Bruchez <olivier@bruchez.org>
2016-02-28 10:43:10 +0100

    Pie chart colors/palette cleanup

---
+++ app/models/Color.scala
@@ -0,0 +1,115 @@
+package models
+
+case class Color(red: Int, green: Int, blue: Int) {
+  assert(red >= 0 && red <= 255)
+  assert(green >= 0 && green <= 255)
+  assert(blue >= 0 && blue <= 255)
+
+  def html: String = f"#$red%02X$green%02X$blue%02X"
+
+  def lighterOrDarker(factor: Double): Color =
+    Color(
+      red = Color.lighterOrDarker(red, factor),
+      green = Color.lighterOrDarker(green, factor),
+      blue = Color.lighterOrDarker(blue, factor))
+
+  def distance(that: Color): Double =
+    math.sqrt(
+      math.pow(this.red - that.red, 2.0) +
+        math.pow(this.green - that.green, 2.0) +
+        math.pow(this.blue - that.blue, 2.0))
+}
+
+object Color {
+  def apply(html: String): Color = {
+    val withoutSharpSymbol = if (html.startsWith("#")) html.substring(1) else html
+
+    assert(withoutSharpSymbol.length == 6)
+
+    Color(
+      red = Integer.parseInt(withoutSharpSymbol.substring(0, 2), 16),
+      green = Integer.parseInt(withoutSharpSymbol.substring(2, 4), 16),
+      blue = Integer.parseInt(withoutSharpSymbol.substring(4, 6), 16))
+  }
+
+  def colors(count: Int): Seq[Color] =
+    algorithm1Colors(count = count)
+    //algorithm2Colors(baseColor = Color("5DA5DA"), count = count)
+    //algorithm3Colors(baseColor = Color("5DA5DA"), count = count)
+
+  private def lighterOrDarker(colorValue: Int, factor: Double): Int = {
+    assert(colorValue >= 0 && colorValue <= 255)
+    (colorValue.toDouble * factor).round.toInt.max(0).min(255)
+  }
+
+  private def algorithm1Colors(count: Int): Seq[Color] =
+    (0 until count).map(i => fixedPalette(i % fixedPalette.size))
+
+  private val fixedPalette = Seq(
+    Color("F15854"), // Red
+    Color("60BD68"), // Green
+    Color("5DA5DA"), // Blue
+    Color("FAA43A"), // Orange
+    Color("B2912F"), // Brown
+    Color("B276B2"), // Purple
+    Color("DECF3F"), // Yellow
+    Color("4D4D4D"), // Gray
+    Color("F17CB0")) // Pink*
+
+  private def algorithm2Colors(baseColor: Color, count: Int): Seq[Color] = {
+    val orderedColors = this.orderedColors(baseColor, count)
+
+    def mostDistant(color: Color, otherColors: Seq[Color]): Color =
+      otherColors.maxBy(color.distance)
+
+    @annotation.tailrec
+    def contrastedColors(acc: List[Color], remaining: List[Color]): List[Color] =
+      if (remaining.isEmpty) {
+        acc.reverse
+      } else {
+        val lastAddedColor = acc.head
+        val mostDistantInRemaining = mostDistant(lastAddedColor, remaining)
+        contrastedColors(mostDistantInRemaining :: acc, remaining.filterNot(_ == mostDistantInRemaining))
+      }
+
+    orderedColors.toList match {
+      case firstColor :: remainingColors =>
+        contrastedColors(acc = List(firstColor), remainingColors)
+      case Nil =>
+        Seq()
+    }
+  }
+
+  private def orderedColors(baseColor: Color, count: Int): Seq[Color] = {
+    import java.awt.{ Color => AwtColor }
+
+    val baseHue :: baseSaturation :: baseBrightness :: Nil =
+      AwtColor.RGBtoHSB(baseColor.red, baseColor.green, baseColor.blue, null).toList
+
+    //println(s"baseHue=$baseHue, baseSaturation=$baseSaturation, baseBrightness=$baseBrightness")
+
+    for {
+      index <- 0 until count
+      hue = (baseHue.toDouble + index.toDouble / count.toDouble * 3.0 / 3.0) % 1.0
+      //_ = { println(s" hue = $hue")}
+      rgb = AwtColor.HSBtoRGB(hue.toFloat, baseSaturation, baseBrightness)
+      awtColor = new AwtColor(rgb)
+    } yield Color(awtColor.getRed, awtColor.getGreen, awtColor.getBlue)
+  }
+
+  private def algorithm3Colors(baseColor: Color, count: Int): Seq[Color] = {
+    /*
+    Idea:
+    - choose randomly count colors, with a constraint on the luminosity (Lmin < L < Lmax)
+    - arrange those count colors in a given order
+       * start with the base color (baseColor)
+       * choose each following color trying to maximize the sum of the distances with all the colors that have already
+         been chosen (maybe with a factor to decrease the importance of the more distance colors)
+    - evaluate the color arrangement using the total sum of the distances between two consecutive colors
+    - repeat several times
+    - chose the color arrangement that maximize the sum of the distances
+     */
+    // @todo
+    Seq()
+  }
+}
--- app/models/PieChart.scala
+++ app/models/PieChart.scala
@@ -17,7 +17,10 @@ object PieChart {
             valueUnit: String,
             values: Seq[(Double, String)],
             maxValues: Int = 10,
-            otherValuesLabel: String = "Other"): PieChart =
+            otherValuesLabel: String = "Other"): PieChart = {
+    val minimalValuesSet = this.minimalValuesSet(values, maxValues, otherValuesLabel)
+    val colors = Color.colors(count = minimalValuesSet.size)
+
     PieChart(
       id = id,
       anchor = anchor,
@@ -25,9 +28,10 @@ object PieChart {
       entityName = entityName,
       valueUnit = valueUnit,
       values =
-          minimalValuesSet(values, maxValues, otherValuesLabel).zipWithIndex.map { case ((value, label), index) =>
-            PieChartValue(value, label, color(index), lighterColor(index))
-          })
+        minimalValuesSet.zipWithIndex.map { case ((value, label), index) =>
+          PieChartValue(value, label, colors(index), colors(index).lighterOrDarker(LighterOrDarkerFactor))
+        })
+  }
 
   private def minimalValuesSet(values: Seq[(Double, String)],
                                maxValues: Int,
@@ -43,51 +47,5 @@ object PieChart {
       valuesToKeep :+ aggregatedValue
     }
 
-  def color(index: Int): Color = colors.apply(index % colors.size)
-  def lighterColor(index: Int): Color = lighterColors.apply(index % lighterColors.size)
-
-  val colors = Seq(
-    Color("F15854"), // Red
-    Color("60BD68"), // Green
-    Color("5DA5DA"), // Blue
-    Color("FAA43A"), // Orange
-    Color("B2912F"), // Brown
-    Color("B276B2"), // Purple
-    Color("DECF3F"), // Yellow
-    Color("4D4D4D"), // Gray
-    Color("F17CB0")) // Pink
-
-  val lighterColors = colors.map(_.lighterOrDarker(factor = 1.1))
-}
-
-case class Color(red: Int, green: Int, blue: Int) {
-  assert(red >= 0 && red <= 255)
-  assert(green >= 0 && green <= 255)
-  assert(blue >= 0 && blue <= 255)
-
-  def html: String = f"#$red%02X$green%02X$blue%02X"
-
-  def lighterOrDarker(factor: Double): Color =
-    Color(
-      red = Color.lighterOrDarker(red, factor),
-      green = Color.lighterOrDarker(green, factor),
-      blue = Color.lighterOrDarker(blue, factor))
-}
-
-object Color {
-  def apply(html: String): Color = {
-    val withoutSharpSymbol = if (html.startsWith("#")) html.substring(1) else html
-
-    assert(withoutSharpSymbol.length == 6)
-
-    Color(
-      red = Integer.parseInt(withoutSharpSymbol.substring(0, 2), 16),
-      green = Integer.parseInt(withoutSharpSymbol.substring(2, 4), 16),
-      blue = Integer.parseInt(withoutSharpSymbol.substring(4, 6), 16))
-  }
-
-  def lighterOrDarker(colorValue: Int, factor: Double): Int = {
-    assert(colorValue >= 0 && colorValue <= 255)
-    (colorValue.toDouble * factor).round.toInt.max(0).min(255)
-  }
+  private val LighterOrDarkerFactor = 1.1
 }
