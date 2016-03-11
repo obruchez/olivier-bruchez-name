b46175cd929474ac6907552062d526ee65c33273
Olivier Bruchez <olivier@bruchez.org>
2016-03-11 22:34:08 +0100

    Ratings / bar charts cleanup

--- app/models/statistics/BarChart.scala
+++ app/models/statistics/BarChart.scala
@@ -1,9 +1,12 @@
 package models.statistics
 
+case class BarChartValueSet[T](values: Seq[T], color: Color)
+
 case class BarChart[T](baseId: String,
                        title: String,
                        datasetLabel: String,
-                       values: Seq[(String, T)])(implicit ev: Numeric[T]) {
+                       labels: Seq[String],
+                       valueSets: Seq[BarChartValueSet[T]])(implicit ev: Numeric[T]) {
   val id = s"chart-$baseId"
   val anchor = baseId
 }
