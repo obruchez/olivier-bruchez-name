b2b81c9eb066ab382e08f0a68881c617f50a6896
Olivier Bruchez <olivier@bruchez.org>
2016-03-12 11:34:14 +0100

    Do not truncate y-axis

--- app/models/statistics/BarOrLineChart.scala
+++ app/models/statistics/BarOrLineChart.scala
@@ -5,7 +5,9 @@ case class BarOrLineChartValueSet[T](values: Seq[T], label: String, color: Color
 case class BarOrLineChart[T](baseId: String,
                              title: String,
                              labels: Seq[String],
-                             valueSets: Seq[BarOrLineChartValueSet[T]])(implicit ev: Numeric[T]) {
+                             valueSets: Seq[BarOrLineChartValueSet[T]],
+                             minYValue: Option[T] = None,
+                             maxYValue: Option[T] = None)(implicit ev: Numeric[T]) {
   val id = s"chart-$baseId"
   val anchor = baseId
 }
