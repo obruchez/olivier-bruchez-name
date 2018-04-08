c5c3bef958fb684dfb8efef9fcbe83126cea8437
Olivier Bruchez <olivier@bruchez.org>
2018-04-08 15:13:41 +0200

    Fix Twitter exception when network connection not available

--- app/twitter/Twitter.scala
+++ app/twitter/Twitter.scala
@@ -6,6 +6,7 @@ import twitter4j._
 import twitter4j.conf.ConfigurationBuilder
 import _root_.util.Configuration
 
+// Make sure the members are lazy in case an Internet connection is not available at initialization
 object Twitter {
   case class ExtendedStatus(status: Status, username: String) {
     def isReply: Boolean = status.getInReplyToStatusId >= 0
@@ -14,22 +15,22 @@ object Twitter {
     def url: URL = new URL(s"https://twitter.com/$username/status/${status.getId}")
   }
 
-  private val configuration = new ConfigurationBuilder()
+  lazy private val configuration = new ConfigurationBuilder()
     .setOAuthConsumerKey(Configuration.string("twitter.consumerkey").get)
     .setOAuthConsumerSecret(Configuration.string("twitter.consumersecret").get)
     .setOAuthAccessToken(Configuration.string("twitter.accesstoken").get)
     .setOAuthAccessTokenSecret(Configuration.string("twitter.accesstokensecret").get)
     .build()
 
-  private val twitter = new TwitterFactory(configuration).getInstance()
+  lazy private val twitter = new TwitterFactory(configuration).getInstance()
 
-  private val user = twitter.showUser(Configuration.string("twitter.username").get)
+  lazy private val user = twitter.showUser(Configuration.string("twitter.username").get)
 
-  val userDescription: String = user.getDescription
+  lazy val userDescription: String = user.getDescription
 
-  val smallProfileImageUrl = new URL(user.getBiggerProfileImageURLHttps)
+  lazy val smallProfileImageUrl = new URL(user.getBiggerProfileImageURLHttps)
 
-  val originalProfileImageUrl = new URL(user.getOriginalProfileImageURLHttps)
+  lazy val originalProfileImageUrl = new URL(user.getOriginalProfileImageURLHttps)
 
   def latestStatuses(count: Int): Seq[ExtendedStatus] = {
     val pageCount = (count.toDouble / MaxStatusCountPerPaging.toDouble).ceil.round.toInt
